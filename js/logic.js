d72e14acbe2b518f4c45863fd0afa396ed175e2e
EricR19 <eric19rr@outlook.es>
2021-11-20 12:05:25 -0600

    Random Word

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,36 @@
 function arrayStrings() {
-  let arrayString = ["heartstone"];
+  let arrayString = [
+    "perro",
+    "gato",
+    "foca",
+    "lobo",
+    "toro",
+    "abeja",
+    "burro",
+    "gallo",
+    "delfin",
+    "zorro",
+    "aguila",
+    "avispa",
+    "ciervo",
+    "delfin",
+    "buitre",
+    "armadillo",
+    "ballena",
+    "bisonte",
+    "elefante",
+    "rinoceronte",
+  ];
+  return arrayString;
 }
+let random = () => {
+  let randomNumber = 0;
+  randomNumber = Math.random() * arrayStrings().length;
+  console.log(Math.floor(randomNumber));
+  return parseInt(randomNumber);
+};
+
+let newGame = () => {
+  let array = arrayStrings();
+  document.getElementById("palabra").innerHTML = array[random()];
+};
