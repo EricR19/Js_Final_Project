4ef48787685fe68c357bfe33be9bcf18fba4712e
Eric <eric19rr@outlook.es>
2021-11-24 21:48:48 -0600

    Compare letters

--- js/logic.js
+++ js/logic.js
@@ -1,55 +1,56 @@
+let global = [];
+
 function arrayStrings() {
-  let arrayString = [
-    "perro",
-    "gato",
-    "foca",
-    "lobo",
-    "toro",
-    "abeja",
-    "burro",
-    "gallo",
-    "delfin",
-    "zorro",
-    "aguila",
-    "avispa",
-    "ciervo",
-    "delfin",
-    "buitre",
-    "armadillo",
-    "ballena",
-    "bisonte",
-    "elefante",
-    "rinoceronte",
-  ];
-  return arrayString;
+    let arrayString = [
+        "perro",
+        "gato",
+        "foca",
+        "lobo",
+        "toro",
+        "abeja",
+        "burro",
+        "gallo",
+        "delfin",
+        "zorro",
+        "aguila",
+        "avispa",
+        "ciervo",
+        "delfin",
+        "buitre",
+        "armadillo",
+        "ballena",
+        "bisonte",
+        "elefante",
+        "rinoceronte",
+    ];
+    return arrayString;
 }
 let random = () => {
-  let randomNumber = 0;
-  randomNumber = Math.random() * arrayStrings().length;
-  console.log(Math.floor(randomNumber));
-  return parseInt(randomNumber);
+    let randomNumber = 0;
+    randomNumber = Math.random() * arrayStrings().length;
+    console.log(Math.floor(randomNumber));
+    return parseInt(randomNumber);
 };
 
 let newGame = () => {
-  let array = arrayStrings();
-  let word = array[random()];
-  document.getElementById("palabra").innerHTML = word;
-  underline(word);
+    let array = arrayStrings();
+    let word = array[random()];
+    document.getElementById("palabra").innerHTML = word;
+    underline(word);
+
 };
 
 let underline = (word) => {
-  let words = word.split("");
-  console.log(words);
-  let sizeArray = [];
-  for (let i = 0; i < words.length; i++) {
-    sizeArray[i] = "_";
-  }
-  let normalizes = normalize(sizeArray);
-  document.getElementById("hidden").innerHTML = normalizes;
+    let words = word.split("");
+    global = words
+    let sizeArray = [];
+    for (let i = 0; i < words.length; i++) {
+        sizeArray[i] = "_";
+    }
+    document.getElementById("hidden").innerHTML = sizeArray.join(" ");
 };
 
-let normalize = (word) => {
-  let normal = word.toString();
-  normal = normal.replace(/,/g, " ");
-  return normal;
-};
+let letters = (letter) => {
+    let a = letter;
+    console.log(global)
+}
