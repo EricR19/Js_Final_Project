da4cd6f11d14ee98d14f6c47a3678634829bb25e
EricR19 <eric19rr@outlook.es>
2021-11-25 20:40:02 -0600

    Finish guest word

--- js/logic.js
+++ js/logic.js
@@ -1,56 +1,75 @@
 let global = [];
+let wordFormed = [];
 
 function arrayStrings() {
-    let arrayString = [
-        "perro",
-        "gato",
-        "foca",
-        "lobo",
-        "toro",
-        "abeja",
-        "burro",
-        "gallo",
-        "delfin",
-        "zorro",
-        "aguila",
-        "avispa",
-        "ciervo",
-        "delfin",
-        "buitre",
-        "armadillo",
-        "ballena",
-        "bisonte",
-        "elefante",
-        "rinoceronte",
-    ];
-    return arrayString;
+  let arrayString = [
+    "perro",
+    "gato",
+    "foca",
+    "lobo",
+    "toro",
+    "abeja",
+    "burro",
+    "gallo",
+    "delfin",
+    "zorro",
+    "aguila",
+    "avispa",
+    "ciervo",
+    "delfin",
+    "buitre",
+    "armadillo",
+    "ballena",
+    "bisonte",
+    "elefante",
+    "rinoceronte",
+  ];
+  return arrayString;
 }
 let random = () => {
-    let randomNumber = 0;
-    randomNumber = Math.random() * arrayStrings().length;
-    console.log(Math.floor(randomNumber));
-    return parseInt(randomNumber);
+  wordFormed = [];
+  let randomNumber = 0;
+  randomNumber = Math.random() * arrayStrings().length;
+  console.log(Math.floor(randomNumber));
+  return parseInt(randomNumber);
 };
 
 let newGame = () => {
-    let array = arrayStrings();
-    let word = array[random()];
-    document.getElementById("palabra").innerHTML = word;
-    underline(word);
-
+  refresh();
+  let array = arrayStrings();
+  let word = array[random()];
+  underline(word);
 };
 
 let underline = (word) => {
-    let words = word.split("");
-    global = words
-    let sizeArray = [];
-    for (let i = 0; i < words.length; i++) {
-        sizeArray[i] = "_";
-    }
-    document.getElementById("hidden").innerHTML = sizeArray.join(" ");
+  let words = word.split("");
+  global = words;
+  for (let i = 0; i < words.length; i++) {
+    wordFormed[i] = "_";
+  }
+  document.getElementById("hidden").innerHTML = wordFormed.join(" ");
 };
 
 let letters = (letter) => {
-    let a = letter;
-    console.log(global)
-}
+  let simpleLetter = letter;
+  logicGame(simpleLetter);
+  document.getElementById("hidden").innerHTML = wordFormed.join(" ");
+};
+
+let logicGame = (data) => {
+  for (let i = 0; i < global.length; i++) {
+    if (data === global[i] && wordFormed[i] == "_") {
+      wordFormed[i] = data;
+      let r = document.getElementById(data);
+      r.style.cursor = "not-allowed";
+      r.style.backgroundColor = "black";
+    }
+  }
+};
+let refresh = () => {
+  for (let i = 0; i < wordFormed.length; i++) {
+    let z = document.getElementById(wordFormed[i]);
+    z.style.cursor = "pointer";
+    z.style.background = "aquamarine";
+  }
+};
