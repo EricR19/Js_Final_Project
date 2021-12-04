b8771d6ffce6e07753bb09c31fb1439e74b25269
EricR19 <eric19rr@outlook.es>
2021-12-03 19:07:59 -0600

    Insert points

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,9 @@
 let global = [];
 let wordFormed = [];
 let notFormer = [];
+let pointsPlayer = 0;
+let pointsCpu = 0;
+let shift = 0;
 
 function arrayStrings() {
   let arrayString = [
@@ -36,6 +39,7 @@ let random = () => {
 };
 
 let newGame = () => {
+  shift = 0;
   refresh();
   let array = arrayStrings();
   let word = array[random()];
@@ -64,24 +68,50 @@ let logicGame = (data) => {
       let r = document.getElementById(data);
       r.style.cursor = "not-allowed";
       r.style.backgroundColor = "black";
+      r.style.pointerEvents = "none";
     }
   }
   if (!global.toString().includes(data)) {
     let r = document.getElementById(data);
-    r.style.cursor = "not-allowed";
     r.style.backgroundColor = "red";
     notFormer.push(data);
+    var sh = shifts();
   }
+  if (sh == 10) {
+    pointsCpu++;
+    alert("Perdio");
+    newGame();
+    document.getElementById("puntosCpu").innerHTML = pointsCpu;
+  }
+  points();
 };
 let refresh = () => {
   for (let i = 0; i < wordFormed.length; i++) {
     let z = document.getElementById(wordFormed[i]);
-    z.style.cursor = "pointer";
-    z.style.background = "hsl(168, 36%, 52%)";
+    if (wordFormed[i] != "_") {
+      z.style.cursor = "pointer";
+      z.style.background = "hsl(168, 36%, 52%)";
+      z.style.pointerEvents = "auto";
+    }
   }
   for (let z = 0; z < notFormer.length; z++) {
     let a = document.getElementById(notFormer[z]);
     a.style.cursor = "pointer";
     a.style.background = "hsl(168, 36%, 52%)";
   }
+};
+let points = () => {
+  if (JSON.stringify(wordFormed) === JSON.stringify(global)) {
+    alert(
+      `Felicidades gano, la palabra era: ${global.toString().replace(/,/g, "")}`
+    );
+    pointsPlayer++;
+    document.getElementById("puntosJugador").innerHTML = pointsPlayer;
+    newGame();
+  }
+};
+
+let shifts = () => {
+  shift++;
+  return shift;
 };
